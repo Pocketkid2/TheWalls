92887765256df72cd80d86529e1949719f961354
Adam Taylor <afranklint16@gmail.com>
2021-12-10 17:50:51 -0700

    Flesh out arena class a lil bit

--- src/main/java/com/github/pocketkid2/thewalls/Arena.java
+++ src/main/java/com/github/pocketkid2/thewalls/Arena.java
@@ -1,7 +1,38 @@
 package com.github.pocketkid2.thewalls;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Location;
+
+import com.sk89q.worldedit.regions.CuboidRegion;
+
 public class Arena {
 
+	private String name;
+
 	private Status status;
+
+	private CuboidRegion arena;
+
+	private List<CuboidRegion> walls;
+
+	private List<Location> spawns;
+
+	public Arena(String n) {
+		name = n;
+		status = Status.INCOMPLETE;
+		arena = null;
+		walls = new ArrayList<CuboidRegion>();
+		spawns = new ArrayList<Location>();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
 
 }
