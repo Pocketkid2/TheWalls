80e1426692a830363f3fe041874c9c1ed1e29ab4
Adam Taylor <afranklint16@gmail.com>
2021-12-12 20:47:48 -0700

    Now we're talking.
    I don't even know if this works but I hope it does.

--- src/main/java/com/github/pocketkid2/thewalls/Region.java
+++ src/main/java/com/github/pocketkid2/thewalls/Region.java
@@ -1,10 +1,15 @@
 package com.github.pocketkid2.thewalls;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.util.BlockVector;
+
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.regions.CuboidRegion;
 
 public class Region implements ConfigurationSerializable {
 
@@ -12,13 +17,32 @@ public class Region implements ConfigurationSerializable {
 	private BlockVector min;
 	private BlockVector max;
 
-	public Region() {
+	// Creates a region using the WorldEdit CuboidRegion class
+	public Region(CuboidRegion region) {
+		world = BukkitAdapter.adapt(region.getWorld());
+		min = BukkitAdapter.adapt(world, region.getMinimumPoint()).toVector().toBlockVector();
+		max = BukkitAdapter.adapt(world, region.getMaximumPoint()).toVector().toBlockVector();
+	}
 
+	// Creates a region using a serialized map (deserialize)
+	public Region(Map<String, Object> map) {
+		world = Bukkit.getWorld((String) map.get("world"));
+		min = (BlockVector) map.get("min");
+		max = (BlockVector) map.get("max");
 	}
 
 	@Override
 	public Map<String, Object> serialize() {
-		// TODO Auto-generated method stub
-		return null;
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("world", world.toString());
+		map.put("min", min);
+		map.put("max", max);
+		return map;
+	}
+
+	// Converts this region into a WorldEdit region
+	public CuboidRegion getWorldEditRegion() {
+		return new CuboidRegion(BukkitAdapter.adapt(world), BukkitAdapter.asBlockVector(min.toLocation(world)),
+				BukkitAdapter.asBlockVector(max.toLocation(world)));
 	}
 }
