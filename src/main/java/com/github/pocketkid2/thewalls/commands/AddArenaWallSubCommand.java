3b2aa133c7d3eed7cd3f055ffc24a32f337b2519
Adam Taylor <afranklint16@gmail.com>
2022-02-07 11:37:02 -0700

    Create add wall command

---
+++ src/main/java/com/github/pocketkid2/thewalls/commands/AddArenaWallSubCommand.java
@@ -0,0 +1,93 @@
+package com.github.pocketkid2.thewalls.commands;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.github.pocketkid2.thewalls.Arena;
+import com.github.pocketkid2.thewalls.TheWallsPlugin;
+import com.github.pocketkid2.thewalls.TheWallsRegion;
+import com.sk89q.worldedit.IncompleteRegionException;
+import com.sk89q.worldedit.LocalSession;
+import com.sk89q.worldedit.WorldEdit;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.regions.Region;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class AddArenaWallSubCommand extends TheWallsSubCommand {
+
+	public AddArenaWallSubCommand(TheWallsPlugin p) {
+		super(p);
+	}
+
+	@Override
+	public boolean mustBePlayer() {
+		return true;
+	}
+
+	@Override
+	public List<String> names() {
+		return Arrays.asList("addarenawall", "addwall", "aw");
+	}
+
+	@Override
+	public String description() {
+		return "Adds your currently selected WorldEdit region as an arena wall";
+	}
+
+	@Override
+	public int minArguments() {
+		return 1;
+	}
+
+	@Override
+	public int maxArguments() {
+		return 1;
+	}
+
+	@Override
+	public boolean isAdminCommand() {
+		return true;
+	}
+
+	@Override
+	public String usageMessage() {
+		return "addwall/aw <name>";
+	}
+
+	@Override
+	public void execute(CommandSender sender, String label, String[] args) {
+		Player player = (Player) sender;
+		Arena arena = plugin.getGM().getArenaByName(args[0]);
+		if (arena == null) {
+			player.sendMessage(plugin.addPrefix(ChatColor.RED + "The arena " + ChatColor.GRAY + args[0] + ChatColor.RED + " does not exist!"));
+			return;
+		}
+		if (arena.getArenaRegion() == null) {
+			player.sendMessage(plugin.addPrefix(ChatColor.RED + "Please define the arena region first before you add a wall!"));
+			return;
+		}
+		LocalSession session = WorldEdit.getInstance().getSessionManager().getIfPresent(BukkitAdapter.adapt(player));
+		if (session == null) {
+			player.sendMessage(plugin.addPrefix(ChatColor.RED + "Couldn't find WorldEdit session!"));
+			return;
+		}
+		try {
+			Region wallRegion = session.getSelection();
+			Region arenaRegion = arena.getArenaRegion().getWorldEditRegion();
+			if (!arenaRegion.contains(wallRegion.getMaximumPoint()) || !arenaRegion.contains(wallRegion.getMinimumPoint())) {
+				player.sendMessage(plugin.addPrefix(ChatColor.RED + "The wall region can't be outside the arena region!"));
+				return;
+			}
+			arena.addWallRegion(new TheWallsRegion(wallRegion.getBoundingBox()));
+			player.sendMessage(plugin.addPrefix(ChatColor.AQUA + "The cuboid region you have selected is now the arena region for " + ChatColor.GRAY + arena.getName()));
+		} catch (IncompleteRegionException e) {
+			player.sendMessage(plugin.addPrefix(ChatColor.RED + "You haven't selected a WorldEdit region!"));
+			return;
+		}
+	}
+
+}
